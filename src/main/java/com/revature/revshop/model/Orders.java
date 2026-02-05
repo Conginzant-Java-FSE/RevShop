85d29df6fc8097fe585f4cf65fa72f8af3fbb548
Spandana M L <spandanaml07@gmail.com>
2026-02-05 21:54:43 +0530

    Added order class to the model

---
+++ src/main/java/com/revature/revshop/model/Orders.java
@@ -0,0 +1,13 @@
+package com.revature.revshop.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+}
