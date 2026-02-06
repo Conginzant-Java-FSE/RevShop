7af0a2e68344a86fc1d44c7c9a2ba07fe4f0a001
rithwik1473 <rithwik.akkana@gmail.com>
2026-02-06 11:24:44 +0530

    Added Payments and TrackingDetails classes

--- src/main/java/com/revature/revshop/model/TrackingDetails.java
+++ src/main/java/com/revature/revshop/model/TrackingDetails.java
@@ -7,23 +7,29 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "tracking_details")
 public class TrackingDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "tracking_id")
     private Integer trackingId;
+
     @ManyToOne
     @JoinColumn(name = "order_id", nullable = false)
     private Orders order;
+
     @Column(length = 100)
     private String status;
+
     @Column(columnDefinition = "TEXT")
     private String description;
+
     @CreationTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    public TrackingDetails(){
+    public TrackingDetails() {
     }
+
     public TrackingDetails(Integer trackingId, Orders order, String status, String description,
             LocalDateTime updatedAt) {
         this.trackingId = trackingId;
@@ -32,44 +38,44 @@ public class TrackingDetails {
         this.description = description;
         this.updatedAt = updatedAt;
     }
-    public Integer getTrackingId()
-    {
+
+    public Integer getTrackingId() {
         return trackingId;
     }
-    public void setTrackingId(Integer trackingId)
-    {
+
+    public void setTrackingId(Integer trackingId) {
         this.trackingId = trackingId;
     }
-    public Orders getOrder()
-    {
+
+    public Orders getOrder() {
         return order;
     }
-    public void setOrder(Orders order)
-    {
+
+    public void setOrder(Orders order) {
         this.order = order;
     }
-    public String getStatus()
-    {
+
+    public String getStatus() {
         return status;
     }
-    public void setStatus(String status)
-    {
+
+    public void setStatus(String status) {
         this.status = status;
     }
-    public String getDescription()
-    {
+
+    public String getDescription() {
         return description;
     }
-    public void setDescription(String description)
-    {
+
+    public void setDescription(String description) {
         this.description = description;
     }
-    public LocalDateTime getUpdatedAt()
-    {
+
+    public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
-    public void setUpdatedAt(LocalDateTime updatedAt)
-    {
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
     }
 }
