e09a3a68f35195ed3fa78cfc14a1bf4c578aeb17
Harini-JP <harinijpandian@gmail.com>
2026-02-10 21:21:08 +0530

    Added Favorite Repository

---
+++ src/main/java/com/revature/revshop/repository/FavoriteRepository.java
@@ -0,0 +1,23 @@
+package com.revature.revshop.repository;
+
+import com.revature.revshop.model.Favorite;
+import com.revature.revshop.model.Product;
+import com.revature.revshop.model.Buyer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Integer> {
+
+
+    List<Favorite> findByBuyer(Buyer buyer);
+
+    Optional<Favorite> findByBuyerAndProduct(Buyer buyer, Product product);
+
+    void deleteByBuyerAndProduct(Buyer buyer, Product product);
+
+}
