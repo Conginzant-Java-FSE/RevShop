578589e79bad2497e007052b3b3f5c6ac50ed85d
Harini-JP <harinijpandian@gmail.com>
2026-02-10 21:20:20 +0530

    Added Category Repository

---
+++ src/main/java/com/revature/revshop/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.revature.revshop.repository;
+
+import com.revature.revshop.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Optional<Category> findByName(String name);
+
+    boolean existsByName(String name);
+
+}
