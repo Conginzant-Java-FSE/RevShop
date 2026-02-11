e9d1bda8660d6db923cbf41e3b41a6832a4d4e48
vishnu-yadav0 <vishyadavviva84479@gmail.com>
2026-02-11 21:59:32 +0530

    Created AddressDTO with some validation constraints NotBlank annotation

--- src/main/java/com/revature/revshop/dto/AddressDTO.java
+++ src/main/java/com/revature/revshop/dto/AddressDTO.java
@@ -1,4 +1,96 @@
 package com.revature.revshop.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class AddressDTO {
+    @NotBlank(message = "address line is required")
+    private String addressLine;
+
+    @NotBlank(message = "City is required")
+    private String city;
+
+    @NotBlank(message = "State is required")
+    private String state;
+
+    private String country;
+
+    @NotBlank(message = "Zip code is required")
+    private String zipCode;
+
+    private Long userId;
+
+    private Boolean isDefault;
+
+
+    public AddressDTO() {
+    }
+
+
+    public AddressDTO(Long userId, String addressLine, String city, String state, String country, String zipCode,
+                      Boolean isDefault) {
+        this.userId = userId;
+        this.addressLine = addressLine;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.zipCode = zipCode;
+        this.isDefault = isDefault;
+    }
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Boolean getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(Boolean isDefault) {
+        this.isDefault = isDefault;
+    }
 }
