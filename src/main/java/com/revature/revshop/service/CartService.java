2523714605430126975361d828ec5de872bf5289
Sai Bharadwaj Kamrajugadda <148251061+Bharatsolo@users.noreply.github.com>
2026-02-11 10:50:31 +0530

    Crated Service layer for Cart,CartItems,Reviews

---
+++ src/main/java/com/revature/revshop/service/CartService.java
@@ -0,0 +1,37 @@
+package com.revature.revshop.service;
+
+import com.revature.revshop.model.Cart;
+import com.revature.revshop.model.User;
+import com.revature.revshop.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class CartService {
+
+    private final CartRepository cartRepository;
+
+    @Autowired
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public Optional<Cart> getCartByUser(User user) {
+        return cartRepository.findByUser(user);
+    }
+
+    public Cart createCart(User user) {
+        Cart cart = new Cart(user);
+        return cartRepository.save(cart);
+    }
+
+    public Cart saveCart(Cart cart) {
+        return cartRepository.save(cart);
+    }
+
+    public Cart findOrCreateCart(User user) {
+        return cartRepository.findByUser(user)
+                .orElseGet(() -> createCart(user));
+    }
+}
