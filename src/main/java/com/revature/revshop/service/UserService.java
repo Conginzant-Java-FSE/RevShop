ea4c876b69fc469ac95d64475b738dd26dee5ef4
Sai Bharadwaj Kamrajugadda <148251061+Bharatsolo@users.noreply.github.com>
2026-03-10 22:47:08 +0530

    Created delete account and deactivate account features

--- src/main/java/com/revature/revshop/service/UserService.java
+++ src/main/java/com/revature/revshop/service/UserService.java
@@ -132,6 +132,25 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public User deactivateUser(Long userId) {
+        log.info("Deactivating user id={}", userId);
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+        existingUser.setActive(false);
+        return userRepository.save(existingUser);
+    }
+
+    public User deleteUser(Long userId) {
+        log.info("Deleting user id={}", userId);
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+        existingUser.setActive(false);
+        existingUser.setName("Deleted User");
+        existingUser.setEmail("deleted_" + UUID.randomUUID().toString() + "@revshop.com");
+        existingUser.setPhone(null);
+        return userRepository.save(existingUser);
+    }
+
     public boolean existsByEmail(String email) {
         return userRepository.existsByEmail(email);
     }
